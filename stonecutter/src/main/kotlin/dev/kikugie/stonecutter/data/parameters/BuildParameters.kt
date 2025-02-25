ff19c29308faa25f56d24053a0710d34cdcb7093
kikugie <stormikiku@gmail.com>
2025-02-25 21:56:55 +0100

    Add file validation and transformation utilities

--- stonecutter/src/main/kotlin/dev/kikugie/stonecutter/data/parameters/BuildParameters.kt
+++ stonecutter/src/main/kotlin/dev/kikugie/stonecutter/data/parameters/BuildParameters.kt
@@ -3,15 +3,21 @@
 package dev.kikugie.stonecutter.data.parameters
 
 import dev.kikugie.semver.Version
+import dev.kikugie.semver.VersionParser
+import dev.kikugie.stitcher.transformer.Replacements
 import dev.kikugie.stitcher.transformer.Replacements.RegexReplacement
 import dev.kikugie.stitcher.transformer.Replacements.ReplacementPhase
 import dev.kikugie.stitcher.transformer.Replacements.StringReplacement
+import dev.kikugie.stitcher.transformer.TransformParameters
+import dev.kikugie.stitcher.util.RegexSerializer
+import dev.kikugie.stonecutter.AnyVersion
 import dev.kikugie.stonecutter.Identifier
 import dev.kikugie.stonecutter.build.*
-import dev.kikugie.stitcher.util.RegexSerializer
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.UseSerializers
-import kotlin.text.Regex
+import java.nio.file.Path
+import kotlin.io.path.extension
+import kotlin.io.path.invariantSeparatorsPathString
 
 /**
  * Represents the build parameters used by the file processor.
@@ -42,12 +48,19 @@ public data class BuildParameters(
             for (it in string) when {
                 it.phase != phase -> continue
                 from == it.target ->
-                    if (to in it.sources) fail("Entries '%s' and '%s' create a circular dependency:\n%s"
-                        .format(from, to, underline(it.toString(), from)))
-                    else { it.target = to; it.sources += from; match = true }
+                    if (to in it.sources) fail(
+                        "Entries '%s' and '%s' create a circular dependency:\n%s"
+                            .format(from, to, underline(it.toString(), from))
+                    )
+                    else {
+                        it.target = to; it.sources += from; match = true
+                    }
+
                 to == it.target || to in it.sources -> kotlin.run { it.sources += from; match = true }
-                from in it.sources -> fail("Entry '%s' can be replaced by both '%s' and '%s':\n%s"
-                    .format(from, to, it.target, underline(it.toString(), from)))
+                from in it.sources -> fail(
+                    "Entry '%s' can be replaced by both '%s' and '%s':\n%s"
+                        .format(from, to, it.target, underline(it.toString(), from))
+                )
             }
             if (!match) string += ReplacementGraph(phase, from, to)
         }
@@ -69,8 +82,38 @@ public data class BuildParameters(
         constructor(phase: ReplacementPhase, source: String, target: String) : this(phase, target) {
             sources += source
         }
+
         val sources: MutableSet<String> = mutableSetOf()
         fun lock() = StringReplacement(phase, sources.toList(), target)
         override fun toString(): String = "[${sources.joinToString { "'$it'" }}] -> '$target'"
     }
+
+    /**
+     * Checks if the given [file] is valid according to allowed [extensions] and [exclusions].
+     * The project switching task expects files in [exclusions] to start at project's `src/` directory,
+     * so the given path should be relativised accordingly.
+     */
+    public fun checkFile(file: Path): Boolean = file.invariantSeparatorsPathString
+        .let { path -> file.extension in extensions && path !in exclusions }
+
+    /**
+     * Converts required data to the format required by the file transformer.
+     * Parameter [key] specifies the name of the implicit version receiver
+     * (usually `minecraft`, which makes the following checks equal: `if >1.21` and `if minecraft: >1.21`,
+     * but can be overridden by [dev.kikugie.stonecutter.controller.StonecutterController.defaultReceiver]).
+     * Parameter [version] specifies the version for the given [key].
+     */
+    public fun toTransformParameters(version: AnyVersion, key: String): TransformParameters = with(dependencies) {
+        getOrElse(key) { VersionParser.parseLenient(version).value }.let {
+            put(key, it)
+            put("", it)
+        }
+        TransformParameters(swaps, constants, this, toReplacementData())
+    }
+
+    /**Extracts string and regex replacements to an immutable format accepted by the file transformer.*/
+    public fun toReplacementData(): Replacements.ReplacementData = Replacements.ReplacementData(
+        replacements.string.map(ReplacementGraph::lock),
+        replacements.regex.toList()
+    )
 }
