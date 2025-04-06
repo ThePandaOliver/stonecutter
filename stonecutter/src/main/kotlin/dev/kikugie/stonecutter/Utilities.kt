2b4cafd5629afdca2e6e375c37058b6cc8152d4f
kikugie <stormikiku@gmail.com>
2025-04-06 13:34:21 +0200

    Clearly, I own an air fryer

--- stonecutter/src/main/kotlin/dev/kikugie/stonecutter/Utilities.kt
+++ stonecutter/src/main/kotlin/dev/kikugie/stonecutter/Utilities.kt
@@ -21,7 +21,7 @@ public const val BNAN: String = "🍌"
 /**
  * Currently running Stonecutter version, serialised in [TreeModel].
  */
-public const val STONECUTTER: String = "0.7-dev-SNAPSHOT"
+public const val STONECUTTER: String = "0.7-alpha.1"
 
 internal val LENIENT_JSON = Json {
     ignoreUnknownKeys = true
